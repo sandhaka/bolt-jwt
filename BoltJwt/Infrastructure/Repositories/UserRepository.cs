b236cf53329961a9530c9ce521747f48b83f3758
Matteo Tosato <matteo.tosato7@gmail.com>
2017-10-30 19:10:07 +0100

    Adding user repository

--- BoltJwt/Infrastructure/Repositories/UserRepository.cs
+++ BoltJwt/Infrastructure/Repositories/UserRepository.cs
@@ -1,24 +1,61 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
+using BoltJwt.Infrastructure.Context;
 using BoltJwt.Model;
 using BoltJwt.Model.Abstractions;
+using Microsoft.EntityFrameworkCore;
 
 namespace BoltJwt.Infrastructure.Repositories
 {
     public class UserRepository : IRepository<User>
     {
-        public User Add(User role)
+        private readonly IdentityContext _context;
+
+        public UserRepository(IdentityContext context)
         {
-            throw new System.NotImplementedException();
+            _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        public void Update(User role)
+        /// <summary>
+        /// Add a new user to the context
+        /// </summary>
+        /// <param name="item">User</param>
+        /// <returns>User added</returns>
+        public User Add(User item)
         {
-            throw new System.NotImplementedException();
+            return _context.Users.Add(item).Entity;
         }
 
-        public Task<User> GetAsync(int id)
+        /// <summary>
+        /// Mark the entity as 'modified'.
+        /// The entity exists in the database and has been modified on the client.
+        /// SaveChanges should send updates for it.
+        /// </summary>
+        /// <param name="item">User</param>
+        public void Update(User item)
         {
-            throw new System.NotImplementedException();
+            _context.Entry(item).State = EntityState.Modified;
+        }
+
+        /// <summary>
+        /// Return the user by id with his properties
+        /// </summary>
+        /// <param name="id">User id</param>
+        /// <returns>User</returns>
+        public async Task<User> GetAsync(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user != null)
+            {
+                await _context.Entry(user)
+                    .Collection(i => i.UserGroups).LoadAsync();
+                await _context.Entry(user)
+                    .Collection(i => i.UserRoles).LoadAsync();
+                await _context.Entry(user)
+                    .Collection(i => i.Authorizations).LoadAsync();
+            }
+
+            return user;
         }
     }
 }
--- BoltJwt/Model/Abstractions/IRepository.cs
+++ BoltJwt/Model/Abstractions/IRepository.cs
@@ -4,8 +4,8 @@ namespace BoltJwt.Model.Abstractions
 {
     public interface IRepository<T>
     {
-        T Add(T role);
-        void Update(T role);
+        T Add(T item);
+        void Update(T item);
         Task<T> GetAsync(int id);
     }
 }
