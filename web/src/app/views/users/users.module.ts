6604ab03e6a10dd4173763cfe8bf4349d59522e6
Matteo Tosato <matteo.tosato7@gmail.com>
2017-11-29 17:58:35 +0100

    Adding ngx-datatable

--- web/src/app/views/users/users.module.ts
+++ web/src/app/views/users/users.module.ts
@@ -5,6 +5,9 @@ import {CommonModule} from "@angular/common";
 import {ModalModule} from "ngx-bootstrap";
 import {ModalComponent} from "../../shared/modals/modal.component";
 import {SharedModule} from "../../shared/shared.module";
+import {UsersRoutingModule} from "./users.routing";
+import {UsersService} from "./users.service";
+import {NgxDatatableModule} from "@swimlane/ngx-datatable";
 
 @NgModule({
   imports: [
@@ -12,11 +15,16 @@ import {SharedModule} from "../../shared/shared.module";
     HttpClientModule,
     ReactiveFormsModule,
     ModalModule.forRoot(),
+    UsersRoutingModule,
+    NgxDatatableModule,
     SharedModule
   ],
   declarations: [],
   entryComponents: [
     ModalComponent
+  ],
+  providers: [
+    UsersService
   ]
 })
 export class UsersModule { }
