cd6fc4dd075168375f91e9e4e7e7cb93ffc810d9
Matteo Tosato <matteo.tosato7@gmail.com>
2017-12-04 11:07:25 +0100

    start data-table filters

---
+++ web/src/app/shared/datatable/filters/data-table-filters.component.ts
@@ -0,0 +1,9 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-data-table-filters',
+  templateUrl: 'data-table-filters.component.html'
+})
+export class DataTableFiltersComponent {
+
+}
