e7ecedadec0bff20faca16d4681424cee5e428fa
Matteo Tosato <matteo.tosato7@gmail.com>
2017-12-06 16:36:27 +0100

    Setup compressing on deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -9,5 +9,14 @@ rm -r ../BoltJwt/wwwroot/*
 # Include it into the project
 cp ../web/dist/* ../BoltJwt/wwwroot/
 
+# Cleaning
+rm -r ../BoltJwt/obj/docker/*
+
 # Build backend and publish the project to the build docker folder
 (cd ../BoltJwt && dotnet publish -o obj/docker)
+
+# Compression
+(cd ../BoltJwt/obj/docker && tar -zcvf release.tar .)
+
+# Output file:
+(cd ../BoltJwt/obj/docker; echo 'Deploy done - Output file: '; eval 'ls -al')
