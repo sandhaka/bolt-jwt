eebc37d1eca8f1dd77a8d6d640e0b5f3494456f8
Matteo Tosato <matteo.tosato7@gmail.com>
2018-01-07 11:56:21 +0100

    Fix file missing

---
+++ BoltJwt/Infrastructure/Repositories/Exceptions/EntityInUseException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BoltJwt.Infrastructure.Repositories.Exceptions
+{
+    public class EntityInUseException : Exception
+    {
+        public string Value { get; }
+
+        public EntityInUseException(string val)
+        {
+            Value = val;
+        }
+    }
+}
