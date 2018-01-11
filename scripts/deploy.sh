256b78a9920e6f7051399efe5f0eb033575a2f39
Matteo Tosato <matteo.tosato7@gmail.com>
2018-01-11 17:18:55 +0100

    Force docker pull on deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@
 scp -i rsa/deploy_rsa.pem ../docker-compose.yml root@94.177.201.179:~/deployed/BoltJwt/
 
 # Run 
-ssh -i rsa/deploy_rsa.pem root@94.177.201.179 'docker-compose -f ~/deployed/BoltJwt/docker-compose.yml up --build -d'
+ssh -i rsa/deploy_rsa.pem root@94.177.201.179 'docker-compose -f ~/deployed/BoltJwt/docker-compose.yml pull && docker-compose -f ~/deployed/BoltJwt/docker-compose.yml up -d'
