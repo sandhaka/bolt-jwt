b84188973361310fb4da49738084f4e91e21be2e
Matteo Tosato <matteo.tosato7@gmail.com>
2018-07-19 22:25:59 +0200

    Deploy fix

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,4 +7,4 @@
 scp -i rsa/deploy_rsa.pem ../docker-compose.yml root@94.177.201.179:~/deployed/BoltJwt/
 
 # Run 
-ssh -i rsa/deploy_rsa.pem root@94.177.201.179 'SQL_CONNECTION_STRING="Server=sql.data,1433;User Id=sa;Password=Password&1;Initial Catalog=boltJwt.data" SQL_PASSWORD="Password&1" docker-compose -f ~/deployed/BoltJwt/docker-compose.yml pull && docker-compose -f ~/deployed/BoltJwt/docker-compose.yml -up -d'
+ssh -i rsa/deploy_rsa.pem root@94.177.201.179 'docker-compose -f ~/deployed/BoltJwt/docker-compose.yml pull && SQL_CONNECTION_STRING="Server=sql.data,1433;User Id=sa;Password=Password&1;Initial Catalog=boltJwt.data" SQL_PASSWORD="Password&1" docker-compose -f ~/deployed/BoltJwt/docker-compose.yml up -d'
